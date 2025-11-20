5d14e15d80353f8d1678cd256f51bbf2d6526079
ArthurOliveirapassos <arthur.passos@unesp.br>
2025-11-20 10:23:58 -0300

    0.1.0

---
+++ R/example.R
@@ -0,0 +1,8 @@
+#' Example FASTA dataset
+#'
+#' a small FASTA dataset with 100 sequences, included for demonstrate
+#' for purpose this package
+#'
+#'@format a character vector of lenght 322
+#'@source Simulated data with giveExampleFasta function created by Arthur Passos
+"example"
---
+++ R/telomeres_hits_example.R
@@ -0,0 +1,8 @@
+#' Example FASTA dataset
+#'
+#' a small FASTA dataset with hits from example.fasta, included for demonstrate
+#' for purpose this package
+#'
+#'@format a character vector of lenght 322
+#'@source Simulated data with giveExampleFasta function created by Arthur Passos
+"telomere_hits_example"
